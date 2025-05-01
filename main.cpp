2f49b94eed94417866e8205dc6f829080efdc548
Дяна <diana.socolcova@gmail.com>
2025-05-01 12:20:16 +0300

    added multiplication

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@
 int main() {
     std::cout << "5 + 3 = " << add(5, 3) << std::endl;
     std::cout << "5 - 3 = " << subtract(5, 3) << std::endl;
+    std::cout << "5 * 3 = " << multiply(5, 3) << std::endl;
     return 0;
 }
