7e29037eb42917bef9328fe1ced0cae649815ac9
Дяна <diana.socolcova@gmail.com>
2025-05-01 12:04:37 +0300

    Instal commit with calculator files

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "calculator.h"
+
+int main() {
+    std::cout << "5 + 3 = " << add(5, 3) << std::endl;
+    std::cout << "5 - 3 = " << subtract(5, 3) << std::endl;
+    return 0;
+}
